6b7acd0e09c35a4eb38a1c4b1dd8451ed819d6ca
Alejandro Baez <abaez2623@gmail.com>
2023-08-07 13:05:44 -0400

    Finished tests

--- models/index.js
+++ models/index.js
@@ -4,17 +4,17 @@ const Post = require("./Post");
 const Profile = require("./Profile");
 const User = require("./User");
 
-User.hasOne(Profile);
-Profile.belongsTo(User);
+// User.hasOne(Profile);
+// Profile.belongsTo(User);
 
-User.hasMany(Post);
-Post.belongsTo(User);
+// User.hasMany(Post);
+// Post.belongsTo(User);
 
-Post.hasMany(Comment);
-Comment.belongsTo(Post);
+// Post.hasMany(Comment);
+// Comment.belongsTo(Post);
 
-User.belongsToMany(Like, { through: "user_like" });
-Like.belongsToMany(User, { through: "user_like" });
+// User.belongsToMany(Like, { through: "user_like" });
+// Like.belongsToMany(User, { through: "user_like" });
 
 module.exports = {
   Comment,
